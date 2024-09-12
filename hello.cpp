93028bf95525b439af21a0cedbb62c3c2a35293e
Robert Lowe <robertlowe1@acm.org>
2024-09-12 13:50:08 -0500

    counting to 10 (c3)

--- hello.cpp
+++ hello.cpp
@@ -5,4 +5,10 @@ using namespace std;
 int main() 
 {
   cout << "Hello, world" << endl;
+
+  // show off our counting abilities
+  cout << "I can count to 10!" << endl;
+  for(int i=0; i<10; i++) {
+    cout << i << endl;
+  }
 }
