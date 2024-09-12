78a02d55b9bab91b0d201f537a6ae46f3e5b4ff5
Robert Lowe <robertlowe1@acm.org>
2024-09-12 14:00:34 -0500

    I can count to 20 now

--- hello.cpp
+++ hello.cpp
@@ -7,8 +7,8 @@ int main()
   cout << "Hello, world" << endl;
 
   // show off our counting abilities
-  cout << "I can count to 10!" << endl;
-  for(int i=1; i<=10; i++) {
+  cout << "I can count to 20!" << endl;
+  for(int i=1; i<=20; i++) {
     cout << i << endl;
   }
 }
